c7916bdc1eda2d4893d45eb49398b5917305f5ff
Paul A. Tumwine <paulatumwine@gmail.com>
2020-01-17 23:00:54 -0600

    Include both optimizations to bubble sort

--- SortingEnv/src/sortroutines/BubbleSort2.java
+++ SortingEnv/src/sortroutines/BubbleSort2.java
@@ -12,40 +12,45 @@ import java.util.Arrays;
  *
  */
 public class BubbleSort2 extends Sorter {
-	
+
 	int[] arr;
+	int swapCount = 0;
+
 	public int[] sort(int[] array){
 		this.arr = array;
 		bubbleSort();
 		return arr;
-		
+
 	}
 	private void bubbleSort(){
-		
+
 		int len = arr.length;
 		for(int i = 0; i < len; ++i) {
-			int nI = len-i-1;
+			int nI = len - i - 1;
 			for(int j = 0; j < nI; ++j) {
 				if(arr[j]> arr[j+1]){
 					swap(j,j+1);
 				}
 			}
+			if (swapCount == 0) break;
+			else swapCount = 0;
 		}
 	}
-	
+
 	int[] swap(int i, int j){
 		int temp = arr[i];
 		arr[i] = arr[j];
 		arr[j] = temp;
+		swapCount++;
 		return arr;
-		
+
 	}
 	public static void main(String[] args){
 		int[] test = {21,13,1,-22, 51, 5, 18};
 		BubbleSort2 bs = new BubbleSort2();
-		
+
 		System.out.println(Arrays.toString(bs.sort(test)));
-		
+
 	}
 
 }
